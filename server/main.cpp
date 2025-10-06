82cbde2fb320d931cd9808e752ecd27a25c2c66f
Xander McKie <xwmpm4@umsystem.edu>
2025-10-06 08:31:01 -0500

    0.14 err

--- server/main.cpp
+++ server/main.cpp
@@ -1,50 +1,66 @@
-//9/15/25 Create a messaging app similar to discord that is encrypted and has a custom server on my home 
-//implement some feature involving music possibly a music status as well as implementation to listen to music together
-//-and share music from multiple platforms : ex: if someone shares a spotify link but another person chose apple or 
-//-youtube as their service it will give them a link to the song on that platform
-
-//build using linode instead of home pc for better security and eoa
-//Server OS: Ubuntu 22.04 LTS
-/*
-Server Language: C++17/20
-
-WebSocket/HTTP Library: uWebSockets (or Boost.Beast if you want more low-level control)
-
-Database: SQLite3 (to start) -> PostgreSQL (for advanced features)
-
-Security: Let's Encrypt (TLS), Libsodium (password hashing, maybe E2EE later)
-
-Client: Vanilla HTML, CSS, JavaScript
-
-Process Management: systemd
-
-VPS Provider: DigitalOcean/Linode
-
-*/
-#include <uWebSockets/App.h>
 #include <iostream>
-#include <unordered_map>
+#include <thread>
+#include <vector>
+#include <unordered_set>
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+
+typedef websocketpp::server<websocketpp::config::asio> server;
+
+using websocketpp::connection_hdl;
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+class WebSocketServer {
+public:
+    WebSocketServer() {
+        m_server.init_asio();
+        
+        m_server.set_open_handler(bind(&WebSocketServer::on_open, this, ::_1));
+        m_server.set_close_handler(bind(&WebSocketServer::on_close, this, ::_1));
+        m_server.set_message_handler(bind(&WebSocketServer::on_message, this, ::_1, ::_2));
+    }
+    
+    void on_open(connection_hdl hdl) {
+        std::cout << "Client connected!" << std::endl;
+        m_connections.insert(hdl);
+    }
+    
+    void on_close(connection_hdl hdl) {
+        std::cout << "Client disconnected!" << std::endl;
+        m_connections.erase(hdl);
+    }
+    
+    void on_message(connection_hdl hdl, server::message_ptr msg) {
+        std::cout << "Received: " << msg->get_payload() << std::endl;
+        
+        // Echo the message back to all clients
+        for (auto conn : m_connections) {
+            m_server.send(conn, msg->get_payload(), msg->get_opcode());
+        }
+    }
+    
+    void run(uint16_t port) {
+        std::cout << "Starting server on port " << port << std::endl;
+        m_server.listen(port);
+        m_server.start_accept();
+        m_server.run();
+    }
+
+private:
+    server m_server;
+    std::unordered_set<connection_hdl, std::hash<connection_hdl>> m_connections;
+};
 
 int main() {
-    std::cout << "Starting server...\n";
-
-    uWS::App().ws<std::string>("/*", {
-        .open = [](auto *ws) {
-            std::cout << "Client connected!\n";
-        },
-        .message = [](auto *ws, std::string_view message, uWS::OpCode opCode) {
-            std::cout << "Received: " << message << "\n";
-            ws->send(message, opCode); // Echo the message back
-        },
-        .close = [](auto *ws, int code, std::string_view message) {
-            std::cout << "Client disconnected!\n";
-        }
-    }).listen(3000, [](auto *listen_socket) {
-        if (listen_socket) {
-            std::cout << "Server is LIVE on port 3000!\n";
-        }
-    }).run();
-
-    std::cout << "Server stopped.\n";
+    try {
+        WebSocketServer server;
+        server.run(3000);
+    } catch (websocketpp::exception const & e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    } catch (...) {
+        std::cout << "Unknown error" << std::endl;
+    }
     return 0;
 }
